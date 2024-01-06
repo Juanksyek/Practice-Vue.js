370da928ea9d8b7a4f8334045cd593464d8f8730
Juan Flores <elgalleto12393@gmail.com>
2024-01-06 01:18:04 -0600

    Bases

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Practice-Vue.js
+
 Vue.js practice course
+
+# post
+
+## Project setup
+
+cd post
+npm install
+
+### Compiles and hot-reloads for development
+
+npm run serve
+
+### Compiles and minifies for production
+
+npm run build
+
+### Lints and fixes files
+
+npm run lint
+
+### Customize configuration
+
+See [Configuration Reference] (https://cli.vuejs.org/config/).
---
+++ post/src/components/New-onef.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Hello {{ email }}</h1>
+  <h1>Mobile: {{ mobile }}</h1>
+  <h1>Name: {{ getName1("Juan") }}</h1>
+  <h1>Name 2: {{ getName1("Carlos") }}</h1>
+  <h1>Data: {{ getData().phone }}</h1>
+  <button v-on:click="fun()">Click here</button>
+  <h1 v-on:mousemove="getConsole('')">Home page </h1>
+</template>
+<script>
+export default {
+  name: "PAGES",
+  data() {
+    return {
+      email: "abc@gmail.com",
+      mobile: "55555",
+    };
+  },
+  methods: {
+    getName1(name){
+      return name;
+    },
+    getData(){
+      return{
+        email: "abc.gmail.com",
+        phone: "99999999",
+      };
+    },
+    fun(){
+      alert("How are you");
+    },
+    getConsole(){
+      console.error("Function is running");
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
+  
